de80f66474f46281b720b8ee4642e63e3e42408e
Danahirmt <51727910+Danahirmt@users.noreply.github.com>
2020-09-30 14:22:57 -0500

    Add files via upload

---
+++ carga_datos.py
@@ -0,0 +1,7 @@
+
+import pandas as pd
+clientes = pd.read_csv('data/cliente_tabla.csv')
+producto = pd.read_csv('data/producto_tabla.csv')
+town_state = pd.read_csv('data/town_state_small.csv')
+train_small = pd.read_csv('data/df_[candidate]_small.csv')
+test_small = pd.read_csv('data/df_[test]_small.csv')
